7676fcc2a1273e9deec8c6602d0bb83254bbf195
mdsauce <max.dobeck@saucelabs.com>
2019-02-15 13:25:55 -0800

    Added better logging names and DATETIME stamps.  Was not getting a unique enough time.

--- launch-sc-serial.sh
+++ launch-sc-serial.sh
@@ -3,8 +3,6 @@ DEFAULT_SC_PATH=/Users/maxdobeck/workspace/sauce_connect/sc-4.5.1-osx
 DEFAULT_SC_VER=4.5.1
 SC_VER=''
 SC_PATH=''
-LOG_PATH=/tmp/sc-five-$(date +"%Y-%m-%d_%H-%M-%S").log
-PID_FILE=/tmp/sc-five-$(date +"%Y-%m-%d_%H-%M-%S").pid
 
 # setVersion declares the version and sets it for later use by the shell
 setVersion () {
@@ -31,18 +29,23 @@ touch /tmp/tunnel-count
 # Launch X number of tunnels in serial
 while [ $i -lt 2 ]
 do
+    DATETIME=$(date +%H-%M-%S)
+    LOG_PATH=/tmp/sc-serial-$DATETIME.log
+    PID_FILE=/tmp/sc-serial-$DATETIME.pid
+    TUNNEL_NAME=sc-serial-$DATETIME
     i=$[$i+1]
+    echo "\n"
     echo "Launching Sauce Connect ${SC_VER}"
     echo Logging to $LOG_PATH
     # SC Command line should evaluate as if you typed it in yourself
-    $SC_PATH/bin/sc -u $SAUCE_USERNAME -k $SAUCE_ACCESS_KEY -v -d $PID_FILE -l $LOG_PATH --se-port 0 | 
+    $SC_PATH/bin/sc -u $SAUCE_USERNAME -k $SAUCE_ACCESS_KEY -i $TUNNEL_NAME -v -d $PID_FILE -l $LOG_PATH --se-port 0 | 
     while IFS= read -r line
     do
         # Read every line as it appears in the STDOUT you would normally see in your terminal
         case $line in
         *"Sauce Connect is up, you may start your tests."* )
             # SC finished successfully starting, let's stop it
-            echo $'\xE2\x9C\x94'  "Sauce Connect started normally"
+            echo $'\xE2\x9C\x94'  "Sauce Connect ${TUNNEL_NAME} started normally"
             echo "Stopping  the tunnel"
             SC_PID=$(cat $PID_FILE)
             cleanup
@@ -57,6 +60,7 @@ do
             ;;
         esac
     done
+    sleep 2
 done
 tunnels_opened=$(wc -l < /tmp/tunnel-count)
 echo "${tunnels_opened}/${i} tunnels started"
